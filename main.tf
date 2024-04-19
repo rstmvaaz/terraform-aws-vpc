33114ee8ea4944c67add19b308643cba06523827
rstmvaaz <r.aziizzzaa@gmail.com>
2024-04-19 00:49:42 +0000

    new

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ resource "aws_subnet" "main" {
   availability_zone = "${var.region}a"
 
   tags = {
-    Name = "subnet1"
+    Name = var.subnet1_name 
   }
 }
 
@@ -24,7 +24,7 @@ resource "aws_subnet" "main2" {
   availability_zone = "${var.region}b"
 
   tags = {
-    Name = "subnet2"
+    Name = var.subnet2_name 
   }
 }
 
@@ -35,7 +35,7 @@ resource "aws_subnet" "main3" {
   availability_zone = "${var.region}c"
 
   tags = {
-    Name = "subnet3"
+    Name = var.subnet3_name 
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -25,8 +25,25 @@ variable subnet3_cidr {
 
 variable ip_on_launch {
   type = bool
+  description = "true or false"
 }
 
 variable instance_type {
   type = string
+  description = "Provide Instance type"
+}
+
+variable subnet1_name {
+  type = string
+  description = "Provide subnet1 name"
+}
+
+variable subnet2_name {
+  type = string
+  description = "Provide subnet2 name"
+}
+
+variable subnet3_name {
+  type = string
+  description = "Provide subnet3 name"
 }
